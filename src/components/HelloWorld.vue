265e4d6049e243cd0421aca728e45462b4565d5b
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-02-02 14:28:47 +0100

    fetch people data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="p in people" :key="p.id">
+    <h2>{{ p.id }} - {{ p.firstname }}
+      - {{ p.lastname }}
+      - {{ p.email }}
+      - {{ p.address.city }}
+      - {{ p.address.country }}
+    </h2>
+  </div>
+  </div>
+
+  <div> 
+    <table id="tableComponent" class="table table-bordered table-striped">
+  <thead>
+    <tr>
+      <!-- loop through each value of the fields to get the table header -->
+      <th  v-for="field in fields" :key='field' @click="sortTable(field)" > 
+        {{field}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
+       </th>
+    </tr>
+  </thead>
+  <tbody>
+      <tr v-for="person in people" :key='person.id'>
+      <td> {{ person.id }} </td>
+      <td> {{ person.firstname }}</td>
+      <td> {{ person.lastname }} </td>
+      <td> {{ person.email }} </td>
+      <td> {{ person.address.city }}</td>
+      <td> {{ person.address.country }} </td>
+    </tr>
+  </tbody>
+</table> 
   </div>
 </template>
 
@@ -35,6 +39,24 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted () {
+    this.getPeople()
+  },
+  data () {
+    return {
+      people: []
+  }
+},
+  methods: {
+    getPeople () {
+      fetch('https://fakerapi.it/api/v1/persons?_locale=fr')
+        .then(response => response.json())
+        .then(data => { this.people = data.data })
+        .catch(error => {
+          console.error('Panic at Backedn', error)
+        })
+    }
   }
 }
 </script>
