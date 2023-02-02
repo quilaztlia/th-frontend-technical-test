8855e3e67a32a7d0b93b239e1b94e1dde87616b5
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-02-02 15:52:19 +0100

    adding edition

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,28 +10,58 @@
   </div> -->
   </div>
 
-  <div> 
-    <table id="tableComponent" class="table table-bordered table-striped">
-  <thead>
-    <tr>
-      <!-- loop through each value of the fields to get the table header -->
-      <th  v-for="field in headers" :key='field' > 
-        {{field}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
-       </th>
-    </tr>
-  </thead>
-  <tbody>
-      <tr v-for="person in people" :key='person.id'>
-      <td> {{ person.id }} </td>
-      <td> {{ person.firstname }}</td>
-      <td> {{ person.lastname }} </td>
-      <td> {{ person.email }} </td>
-      <td> {{ person.address.city }}</td>
-      <td> {{ person.address.country }} </td>
-    </tr>
-  </tbody>
-</table> 
+  <div>
+    <table id="tableComponent">
+      <thead>
+        <tr>
+          <!-- loop through each value of the fields to get the table header -->
+          <th v-for="field in headers" :key='field'>
+            {{ field }} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="person in people" :key='person.id'>
+          <td> {{ person.id }} </td>
+          <td> {{ person.firstname }}</td>
+          <td> {{ person.lastname }} </td>
+          <td> {{ person.email }} </td>
+          <td> {{ person.address.city }}</td>
+          <td> {{ person.address.country }} </td>
+          <td><a href="#" @click="editPerson(person)">Edit</a> </td>
+          <td><a href="#" class="btn btn-danger" @click="deletePerson(person)">Delete</a></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+
+<div v-if="showEditPersonForm">
+  <h2>Edit Person</h2>
+  <form v-on:submit.prevent="update(person)">
+    <div class="form-group">
+      <label for="firstname">First Name: </label>
+      <input type="text" name="firstname" placeholder="Enter First Name" v-model="form.firstname">
+    </div>
+    <div class="form-group">
+      <label for="lastname">Last Name: </label>
+      <input type="text" name="lastname" placeholder="Enter Last Name" v-model="form.lastname">
+    </div>
+    <div class="form-group">
+      <label for="email">Email: </label>
+      <input type="text" name="email" placeholder="Email" v-model="form.email">
+    </div>
+    <div class="form-group">
+      <label for="addresse.city">: </label>
+      <input type="text" name="addresse.city" placeholder="Address.City" v-model="form.address.city">
+    </div>
+    <div class="form-group">
+      <label for="addresse.country">: </label>
+      <input type="text" name="addresse.country" placeholder="Address.City" v-model="form.address.city">
+    </div>
+  <button type="submit" class="btn btn-primary">Update</button>
+</form>
+</div>
+
 </template>
 
 <script>
@@ -40,18 +70,28 @@ export default {
   props: {
     msg: String
   },
-  mounted () {
+  mounted() {
     this.getPeople()
     this.setTableHeaders()
   },
-  data () {
+  data() {
     return {
+      showEditPersonForm: false,
       people: [],
-      headers: []
-  }
-},
+      headers: [],
+      form:{
+        firstname: '',
+        lastname: '', 
+        email: '' ,
+        address:{
+          city: '',
+          country: ''
+        }
+      }      
+    }
+  },
   methods: {
-    getPeople () {
+    getPeople() {
       fetch('https://fakerapi.it/api/v1/persons?_locale=fr')
         .then(response => response.json())
         .then(data => { this.people = data.data })
@@ -59,8 +99,24 @@ export default {
           console.error('Panic at Backedn', error)
         })
     },
-    setTableHeaders(){
-      this.headers = ['Id', 'FirstName', 'LastName', 'Email', 'Address-City', 'Address-Country' ]
+    setTableHeaders() {
+      this.headers = ['Id', 'FirstName', 'LastName', 'Email', 'Address-City', 'Address-Country']
+    },
+    editPerson(selectedPerson) {
+      this.showEditPersonForm = true
+      this.form.id = selectedPerson.id
+      this.form.firstname = selectedPerson.firstname
+      this.form.lastname = selectedPerson.lastname
+      this.form.email = selectedPerson.email
+      this.form.address.city = selectedPerson.city
+
+      console.log(selectedPerson)
+    },
+    deletePerson() {
+
+    },
+    updatePerson(personToUpdate){
+      console.log(personToUpdate)
     }
   }
 }
@@ -71,14 +127,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
