eec268564632aa592cea89aa5af4a9ef54f7de17
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-02-03 10:01:25 +0100

    wip: add person

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="hello">
+
+  <div>
+    <button type="submit" @click="showNewPersonForm()">Add Person</button>
+  </div>
+
+  <div class="table">
     <table id="tableComponent">
       <thead>
-        <tr>          
+        <tr>
           <th v-for="field in headers" :key='field'>
             {{ field }} <i aria-label='Sort Icon'></i>
           </th>
@@ -23,39 +28,65 @@
     </table>
   </div>
 
-<div v-if="showEditPersonForm || showAddNewPersonForm">
-  <h2>Edit Person</h2>
-  <form v-on:submit.prevent="updatePerson(person)">
-    <div class="form-group">
-      <label for="firstname">First Name: </label>
-      <input type="text" name="firstname" placeholder="Enter First Name" v-model="form.firstname">
-    </div>
-    <div class="form-group">
-      <label for="lastname">Last Name: </label>
-      <input type="text" name="lastname" placeholder="Enter Last Name" v-model="form.lastname">
-    </div>
-    <div class="form-group">
-      <label for="email">Email: </label>
-      <input type="text" name="email" placeholder="Email" v-model="form.email">
-    </div>
-    <div class="form-group">
-      <label for="addresse.city">Address.City: </label>
-      <input type="text" name="address.city" placeholder="Address.City" v-model="form.address.city">
-    </div>
-    <div class="form-group">
-      <label for="addresse.country">Address.Country: </label>
-      <input type="text" name="address.country" placeholder="Address.Country" v-model="form.address.country">
-    </div>
-    <div>
-      <button v-if="showAddNewPersonForm" type="submit" @click="addPerson()">Add</button>  
-    </div>
-    <div>
-      <button v-if="showEditPersonForm" type="submit" @click="updatePerson()">Update</button>
-    </div>
-  
-</form>
-</div>
+  <div v-if="showEditPersonForm">
+    <h2>Edit Person</h2>
+    <form v-on:submit.prevent="updatePerson(person)">
+      <div class="form-group">
+        <label for="firstname">First Name: </label>
+        <input type="text" name="firstname" placeholder="Enter First Name" v-model="form.firstname">
+      </div>
+      <div class="form-group">
+        <label for="lastname">Last Name: </label>
+        <input type="text" name="lastname" placeholder="Enter Last Name" v-model="form.lastname">
+      </div>
+      <div class="form-group">
+        <label for="email">Email: </label>
+        <input type="text" name="email" placeholder="Email" v-model="form.email">
+      </div>
+      <div class="form-group">
+        <label for="addresse.city">Address.City: </label>
+        <input type="text" name="address.city" placeholder="Address.City" v-model="form.address.city">
+      </div>
+      <div class="form-group">
+        <label for="addresse.country">Address.Country: </label>
+        <input type="text" name="address.country" placeholder="Address.Country" v-model="form.address.country">
+      </div>
+      <div>
+        <button v-if="showEditPersonForm" type="submit" @click="updatePerson()">Update</button>
+        <button v-if="showEditPersonForm" type="submit" @click="cancelUpdatePerson()">Cancel</button>
+      </div>
+    </form>
+  </div>
 
+  <div v-if="showAddNewPersonForm">
+    <h2>Add New Person</h2>
+    <form v-on:submit.prevent="addPerson(person)">
+      <div class="form-group">
+        <label for="firstname">First Name: </label>
+        <input type="text" name="firstname" placeholder="Enter First Name" v-model="form.firstname">
+      </div>
+      <div class="form-group">
+        <label for="lastname">Last Name: </label>
+        <input type="text" name="lastname" placeholder="Enter Last Name" v-model="form.lastname">
+      </div>
+      <div class="form-group">
+        <label for="email">Email: </label>
+        <input type="text" name="email" placeholder="Email" v-model="form.email">
+      </div>
+      <div class="form-group">
+        <label for="addresse.city">Address.City: </label>
+        <input type="text" name="address.city" placeholder="Address.City" v-model="form.address.city">
+      </div>
+      <div class="form-group">
+        <label for="addresse.country">Address.Country: </label>
+        <input type="text" name="address.country" placeholder="Address.Country" v-model="form.address.country">
+      </div>
+      <div>
+        <button v-if="showAddNewPersonForm" type="submit" @click="addPerson(person)">Add</button>
+        <button v-if="showAddNewPersonForm" type="submit" @click="cancelAddPerson()">Cancel</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -67,40 +98,42 @@ export default {
     msg: String
   },
 
-  setup(){
+  setup() {
     const newPerson = ref({
-        firstname: '',
-        lastname: '', 
-        email: '' ,
-        address:{
-          city: '',
-          country: ''
-        }
-      });
-      const form = ref({
-        firstname: '',
-        lastname: '', 
-        email: '' ,
-        address:{
-          city: '',
-          country: ''
-        }
-      });
-      return  { newPerson, form}
+      firstname: '',
+      lastname: '',
+      email: '',
+      address: {
+        city: '',
+        country: ''
+      }
+    });
+    const form = ref({
+      firstname: '',
+      lastname: '',
+      email: '',
+      address: {
+        city: '',
+        country: ''
+      }
+    });
+    return { newPerson, form }
   },
 
   mounted() {
     this.getPeople()
     this.setTableHeaders()
   },
+  
   data() {
     return {
       showEditPersonForm: false,
-      showAddNewPersonForm: true,
+      showAddNewPersonForm: false,
       people: [],
-      headers: [],                
+      headers: [],
     }
   },
+
   methods: {
     getPeople() {
       fetch('https://fakerapi.it/api/v1/persons?_locale=fr')
@@ -110,47 +143,66 @@ export default {
           console.error('Panic at Backedn', error)
         })
     },
+
     setTableHeaders() {
       this.headers = ['Id', 'FirstName', 'LastName', 'Email', 'Address.City', 'Address.Country']
     },
 
-    addPerson() {
-      // this.showEditPersonForm = true
+    showNewPersonForm(){
+      this.showEditPersonForm = false
+      this.showAddNewPersonForm = true
+      this.form.firstname= '',
+      this.form.lastname= '',
+      this.form.email= '',
+      this.form.address.city =''
+      this.form.address.country= ''
+    },
+
+    addPerson(person) {
       console.log('addPerson: form')
-      console.log(this.newPerson)
+      console.log(person)
       // this.newPerson.id = this.form.id 
       // this.newPerson.firstname = this.form.firstname
       // this.newPerson.lastname = this.form.lastname
       // this.newPerson.email = this.form.email
       // this.newPerson.address.city = this.form.address.city
       // this.newPerson.address.country = this.form.address.country
-      
+
       // this.people.join(this.newPerson)
       console.log('addPerson: form')
       console.log(this.newPerson)
     },
+    cancelAddPerson() {
+      this.showAddNewPersonForm = false
+    },
 
-    updatePerson(){     
-      console.log('New person')
-            
-      let personId =  this.form.id
+    updatePerson() {
+      console.log('updatePerson')
+
+      let personId = this.form.id
       let editedPersonId = this.people.indexOf(p => p.id == personId)
       let editedPerson = this.people.filter(p => p.id == personId)[0]
-      
-      //
+
       editedPerson.firstname = this.form.firstname
       editedPerson.lastname = this.form.lastname
       editedPerson.email = this.form.email
       console.log(editedPerson)
       editedPerson.address.city = this.form.address.city
-      editedPerson.address.country = this.form.address.country     
-      
+      editedPerson.address.country = this.form.address.country
+
       this.people[editedPersonId] = editedPerson
+      this.showEditPersonForm = false
+    },
+
+    cancelUpdatePerson() {
+      this.showEditPersonForm = false
     },
 
     editPerson(selectedPerson) {
       this.showEditPersonForm = true
-      this.addPerson = false
+      this.showAddNewPersonForm = false
+
+      //this.addPerson = false
       console.log(selectedPerson.id)
       let editedPerson = this.people.filter(p => p.id == selectedPerson.id)[0]
       console.log(editedPerson)
@@ -161,7 +213,6 @@ export default {
       this.form.email = editedPerson.email
       this.form.address.city = editedPerson.address.city
       this.form.address.country = editedPerson.address.country
-      // this.updatePerson()
 
       console.log(selectedPerson)
     },
@@ -170,7 +221,7 @@ export default {
       this.people = this.people.filter(p => p.id != personToDeleteId)
       console.log(this.people)
     }
-    
+
   }
 }
 </script>
