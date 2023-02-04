71d6ba477301f5dd75c7b557e8f2aae1661bd5f2
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2023-02-04 13:02:23 +0100

    EditPerson: add template

--- src/views/EditPerson.vue
+++ src/views/EditPerson.vue
@@ -1,8 +1,98 @@
 <template>
-  <h2>Edit Person</h2>
+  <div v-if="showEditPersonForm">
+    <h2>Edit Person</h2>
+    <form v-on:submit.prevent="updatePerson(person)">
+      <div class="form-group">
+        <label for="firstname">First Name: </label>
+        <input type="text" name="firstname" placeholder="First Name" v-model="form.firstname">
+      </div>
+      <div class="form-group">
+        <label for="lastname">Last Name: </label>
+        <input type="text" name="lastname" placeholder="Last Name" v-model="form.lastname">
+      </div>
+      <div class="form-group">
+        <label for="email">Email: </label>
+        <input type="text" name="email" placeholder="Email" v-model="form.email">
+      </div>
+      <div class="form-group">
+        <label for="addresse.city">Address.City: </label>
+        <input type="text" name="address.city" placeholder="Address.City" v-model="form.address.city">
+      </div>
+      <div class="form-group">
+        <label for="addresse.country">Address.Country: </label>
+        <input type="text" name="address.country" placeholder="Address.Country" v-model="form.address.country">
+      </div>
+      <div>
+        <button v-if="showEditPersonForm" type="submit" @click="updatePerson()">Update</button>
+        <button v-if="showEditPersonForm" type="submit" @click="cancelUpdatePerson()">Cancel</button>
+      </div>
+    </form>
+  </div>
 </template>
+
 <script>
+import { ref } from 'vue'
+
 export default {
-  name: 'EditPerson'
+  name: 'EditPerson',
+  props: {
+    idPerson: Number
+  },
+
+  setup() {
+    const form = ref({
+      firstname: '',
+      lastname: '',
+      email: '',
+      address: {
+        city: '',
+        country: ''
+      }
+    });
+    return { form }
+  },
+
+  mounted() { },
+
+  data() {
+    return {
+      showEditPersonForm: true
+    }
+  },
+
+  methods: {
+    updatePerson() {
+      let personId = this.form.id
+      let editedPersonId = this.people.indexOf(p => p.id == personId)
+      let editedPerson = this.people.filter(p => p.id == personId)[0]
+
+      editedPerson.firstname = this.form.firstname
+      editedPerson.lastname = this.form.lastname
+      editedPerson.email = this.form.email
+      editedPerson.address.city = this.form.address.city
+      editedPerson.address.country = this.form.address.country
+
+      this.people[editedPersonId] = editedPerson
+      this.showEditPersonForm = false
+    },
+
+    cancelUpdatePerson() {
+      this.showEditPersonForm = false
+    },
+
+    editPerson(selectedPerson) {
+      this.showEditPersonForm = true
+      this.showAddNewPersonForm = false
+
+      let editedPerson = this.people.filter(p => p.id == selectedPerson.id)[0]
+
+      this.form.id = editedPerson.id
+      this.form.firstname = editedPerson.firstname
+      this.form.lastname = editedPerson.lastname
+      this.form.email = editedPerson.email
+      this.form.address.city = editedPerson.address.city
+      this.form.address.country = editedPerson.address.country
+    }
+  }
 }
 </script>
